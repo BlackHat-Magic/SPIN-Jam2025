3af4285ba4d8f46ad65a2857bb036b9a418fe464
BlackHat-Magic <contact@lukeh.xyz>
2025-10-24 07:29:04 +0000

    testing/topdown_controller: add rotation2D component for sprite rotation

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -32,6 +32,9 @@ pub struct Transform {
     pub rot: Quat,
 }
 
+#[derive(Component)]
+pub struct Rotation2D(pub f32); // rotation in radians
+
 impl Default for Transform {
     fn default() -> Self {
         Self {
